9c91d45dc12edd5a0758c0b48501e88eeae4a2d2
Denys <denny@Denyss-MacBook-Pro.local>
2026-09-19 15:36:54 +0200

    finished with salary

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function getSalary(item) {
+  return Number(item.dataset.salary);
+}
+
+function sortList(list) {
+    const items = Array.from(list.querySelectorAll('li'));
+
+    const sorted = items.sort((a, b) => getSalary(b) - getSalary(a));
+
+    list.append(...sorted);
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.querySelectorAll('li'));
+  
+  return items.map(item => ({
+    name: item.textContent.trim(),
+    salary: getSalary(item)
+  }));
+}
+
+
+sortList(list);
+getEmployees(list);
